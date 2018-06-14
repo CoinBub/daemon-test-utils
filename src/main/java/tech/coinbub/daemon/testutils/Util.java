b18d4210d27834a5705f152baf30aab433b0fc15
Thomas Wilson <thomas@thomaspwilson.com>
2018-06-14 16:41:54 -0400

    Add content-type header for json-rpc communication

--- src/main/java/tech/coinbub/daemon/testutils/Util.java
+++ src/main/java/tech/coinbub/daemon/testutils/Util.java
@@ -3,6 +3,7 @@ package tech.coinbub.daemon.testutils;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 public final class Util {
@@ -11,6 +12,9 @@ public final class Util {
     public static Map<String, String> headers(final String username, final String password) {
         final String cred = Base64.getEncoder().encodeToString((username + ":" + password)
                 .getBytes(StandardCharsets.UTF_8));
-        return Collections.singletonMap("Authorization", "Basic " + cred);
+        final Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Basic " + cred);
+        headers.put("Content-Type", "application/json");
+        return headers;
     }
 }
